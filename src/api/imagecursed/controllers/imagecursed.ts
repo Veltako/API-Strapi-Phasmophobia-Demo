ffeeb1a748294b1eb5b07b05e9c001b4af735af5
Veltako <116906054+Veltako@users.noreply.github.com>
2023-06-22 14:56:54 +0200

    backend

---
+++ src/api/imagecursed/controllers/imagecursed.ts
@@ -0,0 +1,7 @@
+/**
+ * imagecursed controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::imagecursed.imagecursed');
---
+++ src/api/imagecursed/services/imagecursed.ts
@@ -0,0 +1,7 @@
+/**
+ * imagecursed service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::imagecursed.imagecursed');
